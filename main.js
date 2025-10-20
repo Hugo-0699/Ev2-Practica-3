046460a5330c5d52c89faa05d8ba46c2675b9bab
Hugo Alejandro Zapata Rosales <hugoalejandrozapatarosales5@gmail.com>
2025-10-19 22:46:49 -0600

    Borrador de la aplicación

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// Codigo de JavaScript 
+// Codigo de JavaScript
+const boton = document.getElementById("botonBuscar");
+const nombre = document.getElementById("nombreBuscar");
+
+const pnombre = document.getElementById("nombrePersonaje");
+const especie = document.getElementById("especie");
+const edad = document.getElementById("edad");
+const fnacimiento = document.getElementById("nacimiento");
+const origen = document.getElementById("origen");
+const vehiculo = document.getElementById("nave");
+const peliculas = document.getElementById("films");
+
+async function buscarPersonajes (){
+}
