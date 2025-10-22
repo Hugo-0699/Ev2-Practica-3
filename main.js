51e0d55c1ddd37cc07718eab71216cbdc0b4ef78
Hugo Alejandro Zapata Rosales <hugoalejandrozapatarosales5@gmail.com>
2025-10-21 23:27:06 -0600

    Actualización de los estilos de la página. Errores corregidos a la hora de traer los datos de la API

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const nombre = document.getElementById("nombreBuscar");
 const pnombre = document.getElementById("nombrePersonaje");
 const altura = document.getElementById("altura");
 const peso = document.getElementById("peso");
+const ojos = document.getElementById("colorOjos");
 const cpelo = document.getElementById("colorPelo");
 const fnacimiento = document.getElementById("fechaNacimiento");
 const genero = document.getElementById("genero");
@@ -21,18 +22,31 @@ async function buscarPersonajes (){
     const personaje = info.results[0];
     if (!personaje) {
         alert("El personaje que ingresaste no existe o no fue encontrado.");
-        pnombre.innerText = altura.innerText = peso.innerText = cpelo.innerText = 
+        pnombre.innerText = altura.innerText = peso.innerText = ojos.innerText = cpelo.innerText = 
         fnacimiento.innerText = genero.innerText = especie.innerText = planeta.innerText = 
-        vehiculo.innerText = starships.innerText = peliculas.innerText = "No encontrado";
+        vehiculo.innerText = starships.innerText = peliculas.innerText = "----";
         return;
     }
 
     pnombre.innerText = personaje.name;
-    altura.innerText = personaje.height;
-    peso.innerText = personaje.mass;
+
+    // Altura en cm
+    if(personaje.height.toLowerCase() !== "unknown" ){
+        altura.innerText = personaje.height + " cm";
+    } else {
+        altura.innerText = "Desconocido";
+    }
+    // Peso en kg
+    if (personaje.mass.toLowerCase() !== "unknown") {
+    peso.innerText = personaje.mass + " kg";
+    } else {
+    peso.innerText = "Desconocido";
+    }
+
+    ojos.innerText = personaje.eye_color;
     cpelo.innerText = personaje.hair_color;
-    fnacimiento.innerText = personaje.birth_year;
     genero.innerText = personaje.gender;
+    fnacimiento.innerText = personaje.birth_year;
     especie.innerText = personaje.species;
     planeta.innerText = personaje.homeworld;
     vehiculo.innerText = personaje.vehicles;
